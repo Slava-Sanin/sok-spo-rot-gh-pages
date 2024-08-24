e3d0cc53e0f452817ea97a232dd5048fc1c00325
Viacheslav Sanin <slava.sanin@gmail.com>
2024-08-24 20:09:15 +0300

    Updates

---
+++ precache-manifest.dad3174bd7b3625563b4898d04c0b6d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663a1d05520f952bbcf0edc984a01b53",
+    "url": "/sok-spo-rot-gh-pages/index.html"
+  },
+  {
+    "revision": "88fc652198eb66296115",
+    "url": "/sok-spo-rot-gh-pages/static/css/2.7d86e4e7.chunk.css"
+  },
+  {
+    "revision": "c458f4bb0b12ebe10e7c",
+    "url": "/sok-spo-rot-gh-pages/static/css/main.e4320e4a.chunk.css"
+  },
+  {
+    "revision": "88fc652198eb66296115",
+    "url": "/sok-spo-rot-gh-pages/static/js/2.08f16b05.chunk.js"
+  },
+  {
+    "revision": "daf5c09afed02c0dcc2b2b7e1cc77c34",
+    "url": "/sok-spo-rot-gh-pages/static/js/2.08f16b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c458f4bb0b12ebe10e7c",
+    "url": "/sok-spo-rot-gh-pages/static/js/main.8caf0d6f.chunk.js"
+  },
+  {
+    "revision": "b77666b6d005725f8820",
+    "url": "/sok-spo-rot-gh-pages/static/js/runtime-main.7bc93209.js"
+  },
+  {
+    "revision": "c89887e9a6692f4baa46575cb0d8a848",
+    "url": "/sok-spo-rot-gh-pages/static/media/arrows4.c89887e9.png"
+  },
+  {
+    "revision": "05a037f3715d913564f075b6aa5fb185",
+    "url": "/sok-spo-rot-gh-pages/static/media/banner.05a037f3.png"
+  },
+  {
+    "revision": "f2044873a9296832e3555b9bd5e5a0c9",
+    "url": "/sok-spo-rot-gh-pages/static/media/spots.f2044873.png"
+  }
+]);
